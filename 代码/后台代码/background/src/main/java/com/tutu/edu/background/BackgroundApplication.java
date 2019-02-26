c45322438401309ca40ac87086fc4f3e673b8278
CharvinMei <1451722741@qq.com>
2019-02-26 16:36:03 +0800

    Charvin 1

--- 代码/后台代码/background/src/main/java/com/tutu/edu/background/BackgroundApplication.java
+++ 代码/后台代码/background/src/main/java/com/tutu/edu/background/BackgroundApplication.java
@@ -3,17 +3,18 @@ package com.tutu.edu.background;
 import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 //微服务feign组件支持
-@EnableFeignClients
+//@EnableFeignClients
 //微服务Hystrix组件支持
 //@EnableHystrixDashboard
 //@EnableEurekaClient
-@EnableCircuitBreaker
+//@EnableCircuitBreaker
 @SpringBootApplication
 public class BackgroundApplication {
 
