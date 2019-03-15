327c790aa89b33d5d9ce9d7a3ca9c661226adc18
13zqn <398915607@qq.com>
2019-03-15 09:58:08 +0800

    wxxcx4.6

--- 代码/微信小程序代码/pages/talk/talk.js
+++ 代码/微信小程序代码/pages/talk/talk.js
@@ -5,14 +5,71 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    isShow:true,
+    isZan:false,
+    isCollect:false,
+    comment:"",
+    comments:[
+      { pic: "../../images/mainPic/8.png", name: "卷个浪", comment:" 不过一道题算了一百遍也没有解出来，多少也有些让人沮丧啊。你现在做的论文是你最喜欢的法国作家吗？让我猜一猜，司汤达？",time:"3小时"},
+      { pic: "../../images/mainPic/9.png", name: "皮皮", comment: "皮皮就很开心", time: "1分钟" }
+    ],
+    zan:2,
+    collection:3
+  },
+  comment:function(){
+    this.setData({
+      isShow:false
+    })
+  },
+  commentInput:function(e){
+    console.log("input:",e)
+    this.setData({
+      comment:e.detail.value
+    })
+  },
+  send: function (e) {
+    var comments_new={
+      pic: "../../images/mainPic/10.png", 
+      name: "gbfsf", 
+      comment: this.data.comment, 
+      time: "1分钟"
+    };
+    var comments = this.data.comments.concat(comments_new);
+    console.log("comments1:"+comments)
+    this.setData({
+      comments:comments,
+      isShow: true
+    })
+    wx.showToast({
+      title: '评论成功',
+      icon: 'success',
+      duration: 2000,
+    })
+  },
+  zan:function(e){
+    console.log(e)
+    this.setData({
+      zan: this.data.zan + 1,
+      isZan:true
+    })
+  },
+  collect: function () {
+    this.setData({
+      collection: this.data.collection + 1,
+      isCollect:true
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    console.log("options:",options)
+    var list = JSON.parse(options.list);
+    this.setData({
+      article:list.content,
+      title:list.title
+    })
   },
 
   /**
