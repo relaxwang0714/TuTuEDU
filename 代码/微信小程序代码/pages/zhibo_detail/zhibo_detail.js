72c2081100a0bf4be522b1404bff5b06d0bfb080
zzzzzzzzzq <347575419@qq.com>
2019-03-20 15:49:26 +0800

    功能完善
    cnm

--- 代码/微信小程序代码/pages/zhibo_detail/zhibo_detail.js
+++ 代码/微信小程序代码/pages/zhibo_detail/zhibo_detail.js
@@ -1,48 +1,18 @@
-// pages/onlinevedio/onlinevedio.js
+// pages/zhibo_detail/zhibo_detail.js
 Page({
-  
+
   /**
    * 页面的初始数据
    */
   data: {
-    navDatas: [
-      { DataId: 0, navData: '简介' }, { DataId: 1, navData: '目录' },
-      { DataId: 3, navData: '评论（99)' }
-      
-    ],
-    isJoin:true,
-    currentTab: 0,
+
   },
-  navbarTap: function (e) {
-    console.log('picker发送选择改变，携带值为', e)
-    this.setData({
-      currentTab: e.currentTarget.dataset.current
-    })
-  },
-  getclass1: function() {
-    console.log('123');
-    wx.navigateTo({
-      url: '../class01/class01'
-    })
-  },
-  join_course:function(){
-    this.setData({
-      isJoin:false
-    })
-    wx.showToast({
-      title: '加入成功',
-      icon: 'success',
-      duration: 3000
-    })
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
-    this.setData({
-      isJoin:options.isJoin
-    })
+
   },
 
   /**
