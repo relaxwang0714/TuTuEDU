4dca876cba7823874d4405df84f01a6dd3d77c5d
CharvinMei <1451722741@qq.com>
2019-03-25 21:51:55 +0800

    去死吧服务外包

--- 代码/后台代码/background/src/main/java/com/tutu/edu/background/BackgroundApplication.java
+++ 代码/后台代码/background/src/main/java/com/tutu/edu/background/BackgroundApplication.java
@@ -1,15 +1,15 @@
 package com.tutu.edu.background;
 
-import com.netflix.ribbon.proxy.annotation.Hystrix;
+//import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 //微服务feign组件支持
-@EnableFeignClients
+//@EnableFeignClients
 //微服务Hystrix组件支持
 //@EnableHystrixDashboard
 //@EnableEurekaClient
